f49692e5f2a79bb940b8ec4e86272f7e052536bb
patagona-trettig <t.rettig@patagona.de>
2020-04-13 01:07:48 +0200

    Fix connection timeout bug

--- MariaDAO.ts
+++ MariaDAO.ts
@@ -15,11 +15,11 @@ export class MariaDAO {
         })
     }
 
-    async simpleQuery(queryString: string): Promise<any> {
+    async withConnection(f: (PoolConnection) => Promise<any>): Promise<any> {
         let conn;
         try {
             conn = await this.pool.getConnection();
-            return conn.query(queryString);
+            return f(conn);
         }  catch (err) {
             throw err;
         } finally {
@@ -28,55 +28,48 @@ export class MariaDAO {
     }
 
     async getAllUsers(): Promise<any> {
-        const connection = await this.pool.getConnection();
-        return connection.query('SELECT * FROM BrettSpiel.User');
+        return this.withConnection(c => c.query('SELECT * FROM BrettSpiel.User'));
     }
 
     async getUserById(userId: number): Promise<any> {
-        const connection = await this.pool.getConnection();
-        return connection.query(
+        return this.withConnection(c => c.query(
             {namedPlaceholders: true, sql: 'SELECT * FROM BrettSpiel.User WHERE user_id=:id'},
             {id: userId}
-        );
+        ));
     }
 
     async getUserByLogginName(name: string): Promise<any> {
-        const connection = await this.pool.getConnection();
-        return connection.query(
+        return this.withConnection(c => c.query(
             {namedPlaceholders: true, sql: 'SELECT * FROM BrettSpiel.User WHERE login_name=:ln'},
             {ln: name}
-        );
+        ));
     }
 
     async removeUser(userId: number): Promise<any> {
-        const connection = await this.pool.getConnection();
-        return connection.query(
+        return this.withConnection(c => c.query(
             {namedPlaceholders: true, sql: 'DELETE FROM BrettSpiel.User WHERE user_id=:id'},
             {id: userId}
-            );
+            ));
     }
 
     async insertUser(user: DBUser): Promise<any> {
-        const connection = await this.pool.getConnection();
-        return connection.query(
+        return this.withConnection(c => c.query(
                 {namedPlaceholders: true, sql: 'INSERT INTO BrettSpiel.User (login_name, display_name, password_hash) VALUES (:ln, :dn, :ph)'},
                 {ln: user.login_name, dn: user.display_name, ph: user.password_hash}
-                );
+                ));
     }
 
     async updateUser(user: DBUser): Promise<any> {
-        const connection = await this.pool.getConnection();
-        return connection.query(
+        return this.withConnection(c => c.query(
             {namedPlaceholders: true, sql: 'UPDATE BrettSpiel.User SET login_name=:ln, display_name=:dn, password_hash=:ph WHERE user_id=:id'},
             {ln: user.login_name, dn: user.display_name, ph: user.password_hash, id: user.user_id}
-        );
+        ));
     }
 
     async findLogin(login_name: string, password_hash: string): Promise<any> {
-        const connection = await this.pool.getConnection();
-        return connection.query(
+        return this.withConnection(c => c.query(
             {namedPlaceholders: true, sql: 'SELECT * FROM BrettSpiel.User WHERE login_name=:ln AND password_hash=:ph'},
             {ln: login_name, ph: password_hash}
-        );
+        ));
     }
 }
