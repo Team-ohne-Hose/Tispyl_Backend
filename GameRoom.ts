bbe0ed7fd51479115635391a105cbb522d3008da
patagona-trettig <t.rettig@patagona.de>
2020-04-19 00:39:06 +0200

    Add game steps

--- GameRoom.ts
+++ GameRoom.ts
@@ -27,28 +27,31 @@ export class GameRoom extends Room<GameState> {
     }
 
     onJoin(client: Client, options?: any, auth?: any): void | Promise<any> {
-        console.log("onJoin was triggered");
         this.playerNames.set(client.id, options.displayName);
+        this.state.addPlayer(client.id, options.displayName);
         console.log('options were: ', options);
         this.broadcast({type: 'JOIN_MESSAGE', content: { message: `[Server] ${this.playerNames.get(client.id)}(${client.id}) joined the game` }});
-
         return undefined;
     }
 
     onLeave(client: Client, consented?: boolean): void | Promise<any> {
         console.log("onLeave was triggered");
-
+        this.state.removePlayer(client.id);
         return undefined;
     }
 
     onMessage(client: Client, data: any): void {
-        console.log("onMessage was triggered", data);
+        console.log('Got: ', data);
         if (data.type === 'CHAT_MESSAGE') {
             this.broadcast({type: 'CHAT_MESSAGE', content: { message: `[${this.playerNames.get(client.id)}] ${data.content.message}` }})
         } else if (data.type === 'ADVANCE_ROUND') {
             this.state.nextRound();
         } else if (data.type === 'ADVANCE_ACTION') {
             this.state.nextAction();
+        } else if (data.type === 'ADVANCE_TURN') {
+            this.state.nextTurn();
+        } else if (data.type === 'SET_STARTING_CONDITIONS') {
+            this.state.startGame();
         } else {
             console.error("Unknown data: ", data);
         }
--- GameState.ts
+++ GameState.ts
@@ -9,28 +9,54 @@ enum Actions {
 export class GameState extends Schema {
 
     @type('number')
-    round: number = 1;
+    round: number = 0;
 
     @type('string')
     turn: string = 'none';
 
     @type('string')
-    action: string = Actions[Actions.ROLL];
+    action: string = Actions[Actions.EXECUTE];
+
+    internalPlayerList: Map<string, string> = new Map<string, string>();
+    turnIndex = 0;
 
     nextRound() {
         this.round += 1;
     }
 
     nextAction() {
-        let newAction = Actions[this.action+1];
+        let newAction = Actions[Actions[this.action]+1];
         if (newAction === undefined) {
             this.action =  Actions[Actions.ROLL];
+            this.nextTurn();
+        } else {
+            this.action =  newAction;
         }
     }
 
+    nextTurn() {
+        this.turnIndex = this.turnIndex + 1;
+        const turnArray =  Array.from( this.internalPlayerList.values() );
+        if (this.turnIndex === turnArray.length) {
+            this.nextRound();
+            this.turnIndex = 0;
+        }
+        this.turn = turnArray[this.turnIndex];
+    }
 
-    setRound( n: number ) {
-        this.round = n;
+    addPlayer(id: string, name: string) {
+        this.internalPlayerList.set(id, name);
+    }
+
+    removePlayer(id: string) {
+        this.internalPlayerList.delete(id)
+    }
+
+    startGame() {
+            this.round = 1;
+            this.turnIndex = 0;
+            this.turn = Array.from( this.internalPlayerList.values() )[0];
+            this.action = Actions[Actions.ROLL];
     }
 
 }
