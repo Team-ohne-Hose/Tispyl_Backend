3a8a943cf4376ed34f58b575a78734b0a7a9acda
Daniel Liebler <DLiebler13@gmail.com>
2021-02-19 17:11:37 +0100

    use direct path for lsof, sbin shouldnt be in path for non-root, remove direct detach

--- deployment/shutdownServer.sh
+++ deployment/shutdownServer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # kill old instances
-lsof -t -i:41920 | ifne kill $(lsof -t -i:41920)
+/usr/sbin/lsof -t -i:41920 | ifne kill $(/usr/sbin/lsof -t -i:41920)
 echo Killed old instances
 
--- deployment/startServer.sh
+++ deployment/startServer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # kill old instances
-lsof -t -i:41920 | ifne kill $(lsof -t -i:41920)
+/usr/sbin/lsof -t -i:41920 | ifne kill $(/usr/sbin/lsof -t -i:41920)
 echo Killed old instances
 
 # cd into Backend root and run Production server
 cd ~/BrettSpiel_Backend
-nohup npm run runProd &
+
+# let action run into timeout instead of direcly detaching
+# directly detaching with & didnt work
+nohup npm run runProd
 echo Started
