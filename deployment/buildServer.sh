7a5fc7eade536007101587722936d83a8b2a34a1
Daniel Liebler <DLiebler13@gmail.com>
2021-02-28 14:54:31 +0100

    Update buildServer.sh

--- deployment/buildServer.sh
+++ deployment/buildServer.sh
@@ -5,7 +5,11 @@ cd ~/BrettSpiel_Backend
 
 # update local repo
 echo updating local repo
-git pull
+git checkout master
+git fetch --tags
+tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo latest tag is $tag
+git checkout $tag
 
 # update npm dependencies
 echo updating npm dependencies
