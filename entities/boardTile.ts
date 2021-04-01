54888bb7add17fc5f0cbf1174f380a0b094fee25
Manuel Stolze <manu.stolze93@yahoo.de>
2021-04-01 21:21:11 +0200

    add constructor to Board Tile

--- entities/boardTile.ts
+++ entities/boardTile.ts
@@ -12,14 +12,23 @@ class BoardTile {
     @Column({ type: 'varchar', length: 65, unique: true, nullable: false })
     public name: string
 
-    @Column({ type: 'varchar', length: 65, unique: true, nullable: false })
+    @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
     public description: string;
+
+    @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
+    public path: string;
 
     @ManyToOne(() => Language, language => language.board_tiles)
     language: Language;
 
     @ManyToOne(type => User, user => user.board_tile)
     public user: User;
+
+    constructor(name: string, description: string, path: string) {
+        this.name = name;
+        this.description = description;
+        this.path = path
+    }
 }
 
 export default BoardTile;
