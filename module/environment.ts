63a54b4844b983488a6960043b21ededb53b099d
Tizian Rettig <tizian.rettig@stud.h-da.de>
2021-04-11 16:34:04 +0200

    Refactor startup script to use async function and remove old config

--- module/environment.ts
+++ module/environment.ts
@@ -1,6 +1,13 @@
-import fs from 'fs'
-
+/**
+ * This class wraps all environment values in order to provide typesafe access to the .env file.
+ * Issues loading the .env file will result in an error.
+ */
 class Environment {
+
+    public static isLoaded: boolean = false;
+
+    /** META */
+    public static NODE_ENV: string = '';
 
     /** ALL DATABASE VARIABLES. */
 
@@ -16,16 +23,16 @@ class Environment {
     /** The name of the database. */
     public static DB_NAME: string = '';
 
-    /** The port of the running server. */
-    public static SERVER_PORT: number =25670;
-
+    /** The port the running server will listen on. */
+    public static SERVER_PORT: number = 25670;
 
     /**
      * The development variable which prevents to trigger external
      * api calls during development.
      */
-    public static DEVELOPMENT: boolean =
-        (process.env.DEVELOPMENT === "true");
+    public static DEVELOPMENT: boolean = process.env.NODE_ENV === "development";
+
+    /** All SECRETS. */
 
     /** The database password. */
     public static DB_SECRET: string = null;
@@ -33,22 +40,24 @@ class Environment {
     /** The JWT secret key. */
     public static JWT_SECRET: string = null;
 
+    /** Fetches the current $PROJECT_ROOT/.env file*/
+    public static loadEnv(): void {
+        if (process.env.NODE_ENV !== undefined) {
+            Environment.isLoaded = true;
 
-    /** All SECRETS. */
+            Environment.NODE_ENV = process.env.NODE_ENV;
 
-
-    public static setup(): void {
-        if (Environment.DEVELOPMENT) {
-
-            Environment.DB_HOST = process.env.DB_HOST
-            Environment.DB_USER = process.env.DB_USER
-            Environment.DB_NAME = process.env.DB_NAME
+            Environment.DB_HOST = process.env.DB_HOST;
+            Environment.DB_USER = process.env.DB_USER;
+            Environment.DB_NAME = process.env.DB_NAME;
             Environment.DB_PORT = Number(process.env.DB_PORT);
 
             Environment.SERVER_PORT = Number(process.env.SERVER_PORT);
 
             Environment.JWT_SECRET = process.env.JWT_SECRET;
             Environment.DB_SECRET = process.env.DB_SECRET;
+        } else {
+            console.error("Failed to load .env file")
         }
     }
 }
