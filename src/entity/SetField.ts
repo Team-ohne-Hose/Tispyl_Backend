2b358fd2bd746f682ba9e0734f66008ed5b47a2a
Manuel Stolze <manu.stolze93@yahoo.de>
2021-04-14 20:40:40 +0200

    linting still a prob...

--- src/entity/SetField.ts
+++ src/entity/SetField.ts
@@ -5,37 +5,37 @@ import TileSet from "./TileSet";
 @Entity()
 @Index(["id", "fieldNumber"])
 class SetField {
-    @PrimaryGeneratedColumn()
-    public id: number;
+  @PrimaryGeneratedColumn()
+  public id: number;
 
-    @ManyToOne(() => TileSet, tileSet => tileSet.fields)
-    public tileSet: TileSet;
+  @ManyToOne(() => TileSet, tileSet => tileSet.fields)
+  public tileSet: TileSet;
 
-    @Column({ type: "int", nullable: false })
-    public fieldNumber: number;
+  @Column({ type: "int", nullable: false })
+  public fieldNumber: number;
 
-    @OneToOne(() => BoardTile, boardTile => boardTile.setField)
-    @JoinColumn()
-    public boardTile: BoardTile;
+  @OneToOne(() => BoardTile, boardTile => boardTile.setField)
+  @JoinColumn()
+  public boardTile: BoardTile;
 
-    @Column({ type: "int", nullable: true })
-    public restrictRing: number;
+  @Column({ type: "int", nullable: true })
+  public restrictRing: number;
 
-    @Column({ type: "int", nullable: true })
-    public restrictField: number;
+  @Column({ type: "int", nullable: true })
+  public restrictField: number;
 
-    @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP(6)" })
-    public createdAt: Date;
+  @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP(6)" })
+  public createdAt: Date;
 
-    @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
-    public updatedAt: Date;
+  @Column({ type: "datetime", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+  public updatedAt: Date;
 
-    constructor(fieldNumber: number, boardTile: BoardTile, restrictRing: number = null, restrictField: number = null) {
-        this.fieldNumber = fieldNumber;
-        this.boardTile = boardTile;
-        this.restrictRing = restrictRing;
-        this.restrictField = restrictField;
-    }
+  constructor(fieldNumber: number, boardTile: BoardTile, restrictRing: number = null, restrictField: number = null) {
+    this.fieldNumber = fieldNumber;
+    this.boardTile = boardTile;
+    this.restrictRing = restrictRing;
+    this.restrictField = restrictField;
+  }
 }
 
 export default SetField;
