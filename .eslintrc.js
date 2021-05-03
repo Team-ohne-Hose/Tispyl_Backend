18b02a3784841b335df5ec3a538a0d17f928efcb
Manuel Stolze <manu.stolze93@yahoo.de>
2021-05-03 14:45:53 +0200

    solved eslint / prettier conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
@@ -12,7 +11,9 @@ module.exports = {
   },
 
   rules: {
+    'prettier/prettier': 'error',
     '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-empty-function': 0,
     'no-console': 'off',
   },
   env: {
