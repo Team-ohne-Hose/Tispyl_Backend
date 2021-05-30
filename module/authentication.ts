2082d32a10ceb1f5e244cce8db95f4eb7100f707
Manuel Stolze <manu.stolze93@yahoo.de>
2021-05-30 12:52:38 +0200

    APIResponses will now be send from the server
    Linting

--- module/authentication.ts
+++ module/authentication.ts
@@ -1,76 +1,89 @@
-import { Request, Response, NextFunction } from "express";
-import { JwtToken, JwtUserData } from "../types/JwtToken"
-import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express';
+import { JwtToken, JwtUserData } from '../types/JwtToken';
+import jwt from 'jsonwebtoken';
 import * as hash from 'object-hash';
-import { APIResponse } from "../model/APIResponse";
+import { APIResponse } from '../model/APIResponse';
 
 class Authentication {
+  /** Number of salt rounds for hashing the password. */
+  private static SALT_ROUNDS = 10;
 
-    /** Number of salt rounds for hashing the password. */
-    private static SALT_ROUNDS: number = 10
+  static JWT_OPTIONS: jwt.SignOptions = {
+    expiresIn: 10 * 12, // Time given in seconds.
+    //expiresIn: 3600 * 12, // Time given in seconds.
+  };
 
-    static JWT_OPTIONS: jwt.SignOptions = {
-        expiresIn: 3600*12 // Time given in seconds.
+  public static async generateJwtToken(
+    userData: JwtUserData,
+    jwtOptions: {}
+  ): Promise<string> {
+    return jwt.sign(userData, process.env.JWT_SECRET, jwtOptions);
+  }
+
+  public static async verifyJwtToken(token: string): Promise<JwtToken | null> {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET) as JwtToken;
+    } catch (error) {
+      console.warn("JWT token couldn't be verified!");
+      return null;
     }
-    public static async generateJwtToken(userData: JwtUserData, jwtOptions: {}): Promise<string> {
-        return jwt.sign(userData, process.env.JWT_SECRET, jwtOptions);
+  }
+
+  /**
+   * Return the decoded JWT token from the request object. If the
+   * request didn't contain a valid authorization header the
+   * function will return null.
+   */
+  public static async getJwtToken(req: Request): Promise<JwtToken | null> {
+    try {
+      const jwtString: string = req.headers.authorization;
+      return jwt.decode(jwtString, { json: true }) as JwtToken;
+    } catch (error) {
+      console.error('Error while extraction JWT token! Error: ', error);
+      return null;
     }
+  }
 
-    public static async verifyJwtToken(token: string): Promise<JwtToken | null> {
-        try {
-            return jwt.verify(token, process.env.JWT_SECRET) as JwtToken
-        } catch (error) {
-            console.warn("JWT token couldn't be verified!");
-            return null;
-        }
-    }
+  public static hashPassword(password: string): string {
+    return hash.MD5(password);
+  }
 
-    /**
-     * Return the decoded JWT token from the request object. If the
-     * request didn't contain a valid authorization header the
-     * function will return null.
-     */
-    public static async getJwtToken(req: Request): Promise<JwtToken | null> {
-        try {
-            const jwtString: string = req.headers.authorization;
-            return jwt.decode(jwtString, { json: true }) as JwtToken;
-        } catch (error) {
-            console.error("Error while extraction JWT token! Error: ", error);
-            return null;
-        }
-    }
+  public static async verifyAccess(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> {
+    const jwtToken: string = req.get('Authorization');
 
-    public static hashPassword(password: string): string {
-        return hash.MD5(password)
-    }
-
-    public static async verifyAccess(req: Request, res: Response, next: NextFunction): Promise<any> {
-        const jwtToken: string = req.get("Authorization");
-
-        // Check if a authorization header is set.
-        if (jwtToken === undefined) {
-            new APIResponse(res, 401, {}, [{
-                'userMessage': "Permission not granted.",
-                'internalMessage': 'Wrong JWT token was provided.',
-            }]);
-            return;
-        }
-
-        // If the header exists check the JWT token.
-        const validToken: JwtToken = await Authentication.verifyJwtToken(jwtToken);
-
-        if (!validToken) {
-            new APIResponse(res, 401, {}, [{
-                'userMessage': "Permission not granted.",
-                'internalMessage': 'Wrong JWT token was provided.',
-            }]);
-            return;
-        }
-
-        // If the token is valid follow the next route.
-        next();
+    // Check if a authorization header is set.
+    if (jwtToken === undefined) {
+      new APIResponse(res, 401, {}, [
+        {
+          userMessage: 'Permission not granted.',
+          internalMessage: 'No JWT token was provided.',
+        },
+      ]).send();
+      return;
     }
 
+    // If the header exists check the JWT token.
+    const validToken: JwtToken = await Authentication.verifyJwtToken(jwtToken);
+
+    console.log(validToken, !null);
+
+    if (validToken === null) {
+      new APIResponse(res, 401, {}, [
+        {
+          userMessage: 'Permission not granted.',
+          internalMessage: 'Wrong JWT token was provided.',
+        },
+      ]).send();
+      return;
+    }
+
+    // If the token is valid follow the next route.
+    next();
+  }
 }
 
 export default Authentication;
