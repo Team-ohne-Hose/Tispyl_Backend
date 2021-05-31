22b61e16fa958a14fd97f18d175697020742f83c
Manuel Stolze <63359026+manuelstolze@users.noreply.github.com>
2021-05-31 20:56:04 +0200

    Expired jwt token (#42)
    * Linting
    * APIResponses will now be send from the server

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,17 +1,21 @@
-import { Router } from 'express'
-import UserController from '../controller/user.controller'
+import { Router } from 'express';
+import UserController from '../controller/user.controller';
 import Authentication from '../../module/authentication';
 
 const userRouter: Router = Router({ mergeParams: true });
 
 /** ALL USER ROUTES. */
 
-userRouter.get("/", Authentication.verifyAccess, UserController.getSingleUser);
-userRouter.get("/all", Authentication.verifyAccess, UserController.getAllUsers);
-userRouter.post("/", UserController.createUser);
-userRouter.patch("/", Authentication.verifyAccess, UserController.updateUser);
+userRouter.get('/', Authentication.verifyAccess, UserController.getSingleUser);
+userRouter.get('/all', Authentication.verifyAccess, UserController.getAllUsers);
+userRouter.post('/', UserController.createUser);
+userRouter.patch('/', Authentication.verifyAccess, UserController.updateUser);
 
-userRouter.delete("/:userId", Authentication.verifyAccess, UserController.deleteUser);
-userRouter.post("/token", UserController.loginUser);
+userRouter.delete(
+  '/:userId',
+  Authentication.verifyAccess,
+  UserController.deleteUser
+);
+userRouter.post('/token', UserController.loginUser);
 
 export default userRouter;
