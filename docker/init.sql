94731ba6dd192e4e882138bdc01a42dc8345984d
Manuel Stolze <manu.stolze93@yahoo.de>
2021-08-04 23:28:08 +0200

    edit init.sql

--- docker/init.sql
+++ docker/init.sql
@@ -3,85 +3,14 @@
 -- Model: New Model    Version: 1.0
 -- MySQL Workbench Forward Engineering
 
+-- EDIT: Manuel Stolze 
+-- Aug 04 2021 23:26 
+
 SET @OLD_UNIQUE_CHECKS=@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema BrettSpiel
--- -----------------------------------------------------
-
--- -----------------------------------------------------
 -- Schema BrettSpiel
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `BrettSpiel` DEFAULT CHARACTER SET utf16 ;
-USE `BrettSpiel` ;
-
--- -----------------------------------------------------
--- Table `BrettSpiel`.`User`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BrettSpiel`.`User` (
-  `user_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `login_name` VARCHAR(45) NOT NULL,
-  `display_name` VARCHAR(45) NOT NULL,
-  `password_hash` VARCHAR(45) NOT NULL,
-  `user_creation` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `time_played` INT(10) UNSIGNED NOT NULL DEFAULT 0,
-  `profile_picture` VARCHAR(45) NULL DEFAULT NULL,
-  `last_figure` INT(10) UNSIGNED NOT NULL DEFAULT 1,
-  `is_connected` TINYINT(1) NOT NULL DEFAULT 0,
-  `is_dev` TINYINT(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY (`user_id`),
-  UNIQUE KEY (`login_name`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf16;
-
-CREATE TABLE IF NOT EXISTS `BrettSpiel`.`Games` (
- `game_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
- `game_name` VARCHAR(45),
- `author` VARCHAR(45),
- `skin` VARCHAR(45),
- `randomizeTiles` TINYINT(1),
- `startTime` DATETIME,
- `endTime` DATETIME,
- `maxPlayers` INT(10),
- `maxRound` INT(10),
- PRIMARY KEY (`game_id`),
- UNIQUE KEY (`game_id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf16;
--- SET SQL_MODE=@OLD_SQL_MODE;
--- SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-
--- -----------------------------------------------------
--- Table: Role
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `role` (
-  `role_id` int(11) NOT NULL COMMENT 'primary key',
-  `create_time` datetime DEFAULT NULL COMMENT 'create time',
-  `update_time` datetime DEFAULT NULL COMMENT 'update time',
-  `description` varchar(65) DEFAULT NULL,
-  PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
-
--- -----------------------------------------------------
--- Table: Role_assignment
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `role_assignment` (
-  `role_id` int(11) NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `create_time` date DEFAULT NULL,
-  `update_time` date DEFAULT NULL,
-  PRIMARY KEY (`role_id`,`user_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `role_assignment_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`),
-  CONSTRAINT `role_assignment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf16;
